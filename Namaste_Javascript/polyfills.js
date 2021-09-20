e7562d36410b16b18f77d2cf951032d767ac7e82
Ashish Kumar Gupta <46626591+ashishugi@users.noreply.github.com>
2021-09-20 16:18:01 +0530

    reduce

---
+++ Namaste_Javascript/polyfills.js
@@ -0,0 +1,39 @@
+// map
+var ar = [1,2,3,4];
+console.log(ar.map((x)=>{
+  return x*2;
+}))
+// myMap
+
+Array.prototype.myMap = function(y){
+  let ar = [];
+  for(let i=0;i<this.length;i++){
+    ar[i] = y(this[i],i,this);
+  }
+  return ar;
+}
+var ar2 = [8,1,2,3,4];
+console.log(ar2.myMap((x)=>{
+  return x*3;
+}))
+
+/////////////////////////// Reduce
+var ar = [1,2,3,4];
+
+console.log(ar.reduce(function(acc,curr){
+  acc+=curr;
+  return acc;
+},0))
+
+Array.prototype.myReduce = function(call,intial){
+  let prev = intial;
+  for(let i=0;i<this.length;i++){
+    prev = call(prev, this[i],i,this);
+  }
+  return prev;
+}
+
+console.log(ar.myReduce(function(acc,curr){
+  acc+=curr;
+  return acc;
+},0))
